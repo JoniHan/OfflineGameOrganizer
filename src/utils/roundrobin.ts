446106abe50074bb2a22897fe7a91ca34e47d24e
JoniHan <joni.hanninen@hotmail.com>
2021-03-31 13:06:59 +0300

    functionality for matchmaking, functionality for placing matches on different devices

--- src/utils/roundrobin.ts
+++ src/utils/roundrobin.ts
@@ -1,3 +1,24 @@
-export const roundRobin = () => {
-    return;
-}
+var robin = require('roundrobin');
+
+export const roundRobinMatches = (pools: string[]) => {
+    const matchesPerPool = pools.map(pool => {
+        const singlePool = pool.split(',');
+        const matchesSinglePool = robin(singlePool.length, singlePool);
+        return matchesSinglePool;
+    });
+    const allRRMatches = allMatches(matchesPerPool);
+    return allRRMatches;
+};
+
+export const allMatches = (pools: [][][]) => {
+    console.log('test');
+    const matchArray: [][] = [];
+    pools.forEach(pool => {
+        pool.forEach(round => {
+            round.forEach(match => {
+                matchArray.push(match);
+            });
+        });
+    });
+    return matchArray;
+};
--- src/views/round-robin-view.tsx
+++ src/views/round-robin-view.tsx
@@ -3,10 +3,12 @@ import { Button } from '../components/button';
 import { Input } from '../components/input';
 import PoolInputGroup from '../components/pool-input-group';
 import PoolContext from '../context/pool';
+import { roundRobinMatches } from '../utils/roundrobin';
 
 export const RoundRobinView = () => {
     const [stationCount, setStationCount] = React.useState(0);
     const [poolCount, setPoolCount] = React.useState(0);
+    const [stationMatches, setStationMatches] = React.useState([['']]);
     const poolContext = React.useContext(PoolContext);
 
     return (
@@ -42,7 +44,22 @@ export const RoundRobinView = () => {
             <PoolInputGroup count={poolCount} />
             <Button onClick={
                 () => {
-                    return;
+                    const stationMatchesArray: string[][] = [];
+                    for (let i = 0; i < stationCount; i++) {
+                        stationMatchesArray.push([]);
+                    };
+                    const matches = roundRobinMatches(poolContext.pools);
+                    let stationId = 0;
+                    matches.forEach((match: string[]) => {
+                        console.log()
+                        if (stationId === stationCount) {
+                            stationId = 0;
+                        }
+                        stationMatchesArray[stationId].push(`${match[0]} VS. ${match[1]}`);
+                        stationId++;
+                    });
+                    setStationMatches(stationMatchesArray);
+                    console.log(stationMatches);
                 }}>
                 {'Calculate'}
             </Button>
