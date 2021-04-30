9f649b448d495fbf3b7271dc09859d23ff41ce8e
JoniHan <joni.hanninen@hotmail.com>
2021-04-30 14:05:21 +0300

    Style changes

--- src/components/match-container.tsx
+++ src/components/match-container.tsx
@@ -13,7 +13,22 @@ const Div = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-round;
+`;
+
+const MatchContainerWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-round;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding: 5px;
+    width: 100%;
+    border: solid 1px rgba(164,164,164,0.58);
+    box-shadow: 4px 4px 8px 0px rgba(164,164,164,0.58);
+    -webkit-box-shadow: 4px 4px 8px 0px rgba(164,164,164,0.58);
+    -moz-box-shadow: 4px 4px 8px 0px rgba(164,164,164,0.58);
 `;
 
 export const MatchContainer: React.FC<IMatchContainerProps> = (props) => {
@@ -23,9 +38,10 @@ export const MatchContainer: React.FC<IMatchContainerProps> = (props) => {
     const elementArray: JSX.Element[] = props.matches.map(
         (match, idx) => {
             return (
-                <Div key={idx}>
+                <MatchContainerWrapper key={idx}>
                     <p>{match.matchCaption}</p>
                     <Button
+                        className={'btn btn-primary btn-sm'}
                         onClick={
                             () => {
                                 matchContext.setCompletedMatches([...matchContext.completedMatches, match.matchId]);
@@ -35,7 +51,7 @@ export const MatchContainer: React.FC<IMatchContainerProps> = (props) => {
                     >
                         {matchContext.completedMatches.indexOf(match.matchId) > -1 ? 'Match completed' : 'Complete match' }
                     </Button>
-                </Div>
+                </MatchContainerWrapper>
             );
         }
     );
